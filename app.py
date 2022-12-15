5b818a9b5d5246da21de34e1655cc722512b0dde
gogrene <gogreneinfo@gmail.com>
2022-12-15 03:17:32 +0100

    dockerfile

--- app.py
+++ app.py
@@ -603,8 +603,8 @@ def create_app(test_config=None):
             'feedback': body
         })
 
-    if __name__ == '__main__':
+    # if __name__ == '__main__':
     #   port = int(os.environ.get('PORT', 8080))
-      app.run(host='0.0.0.0', port=8080)
+    #   app.run(host='0.0.0.0', port=8080)
 
     return app
